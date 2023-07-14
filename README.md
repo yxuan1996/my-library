968f960243f48972fc68ce78dc3aaa1f7c13f20b
yxuan1996 <yxuan1996@gmail.com>
2023-07-14 02:40:23 +0000

    Complete Grid of Cards

--- README.md
+++ README.md
@@ -13,16 +13,19 @@ python -m http.server 8000
 
 ## Development Notes
 
+#### Data Model
+We create a factory function for the Book object. 
+
+New books will be added to a list called `myLibrary`
+
 #### Grid of Cards
-We want to populate the main page with cards of books that we have read. We will use grid and bootstrap / daisyUI cards to do that. 
+We want to populate the main page with cards of books that we have read. We will use grid and tailwind / daisyUI cards to do that. 
 
 We have a JSON list of books, and we will use `.map` to iterate through and list and apply the data to template literals. 
 
 Template literals allow us to pass data into HTML, instead of creating elements individually.
 
 https://stackoverflow.com/questions/67413357/javascript-loop-to-create-list-of-cards
-
-https://stackoverflow.com/questions/63074270/create-a-loop-of-bootstrap-4-card-with-given-array 
 
 #### Add Book Form
 When the `Add Book` button is pressed, we want to show a modal that allows the user to input data about a new book. 
--- app.js
+++ app.js
@@ -1,9 +1,78 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
-}
+const Book = (id, title, author, status) => {
+  return { id, title, author, status };
+};
 
 function addBookToLibrary() {
-  // do stuff here
+  // use unix timestamp as ID
+  let newBook = Book(Date.now(), newtitle, newsauthor, newstatus);
+  myLibrary.push(newBook)
+
+  console.log(myLibrary);
+
 }
+
+function removeBookFromLibrary() {
+
+}
+
+function toggleBookStatus(editbook) {
+
+}
+
+function openModalForm() {
+
+}
+
+function closeModalForm() {
+
+}
+
+
+function setData() {
+
+}
+
+function render() {
+  const bookgrid = document.getElementById('bookgrid');
+  const valuesCards = [{
+      image: '../img/image1.png',
+      title: 'title 1',
+      content: 'super content 1',
+    },
+    {
+      image: '../img/image2.png',
+      title: 'title 2',
+      content: 'super content 2'
+    },
+    {
+      image: '../img/image-3.png',
+      title: 'title3',
+      content: 'blablablablbalbalbabla blablaba'
+    },
+  ]
+
+  // the map function returns a list of objects separated by commas. To remove the commas we use .join('')
+  function returnCards(valuesCards) {
+    return `<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 my-3 mx-3">` + valuesCards.map(valuesCard =>
+
+    `<div class="card bg-neutral text-neutral-content">
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">${valuesCard.title}</h2>
+        <p>${valuesCard.author}</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">${valuesCard.status}</button>
+          <button class="btn btn-ghost">Remove</button>
+        </div>
+      </div>
+    </div>
+    `).join('')
+    + `</div>`;
+  }
+
+  bookgrid.innerHTML = returnCards(valuesCards);
+}
+
+// Initialize Page
+render()
